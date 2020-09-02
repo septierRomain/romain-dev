12978ba0c825b69f748b02c8c6880719488fc86f
Romain <septier.romain@gmail.com>
2020-09-02 11:34:40 +0200

    create content for portfolio card

--- src/Component/Portfolio/index.js
+++ src/Component/Portfolio/index.js
@@ -17,50 +17,69 @@ function Portfolio() {
               <Card.Content header='Kalees' />
               <Card.Content description='Site vitrine dun artiste peintre/graffeur.' />
               <Card.Content extra>
-                <Icon name='react' />ReactJS
+                <div className='techno'>
+                  <Icon name='react' />ReactJS
+                </div>
+                <div className='techno'>
+                  <Icon name='js square' />Javascript ES6
+                </div>
               </Card.Content>
               <Card.Content extra>
                 <a href='https://github.com/Gizmo675/kalees' target='blank'>
                   <Icon name='code' />
                 </a>
-                <a href='https://kalees.netlify.app/' target='blank'>
-                  <Icon name='eye' />
-                </a>
               </Card.Content>
             </Card>
-            <img src='https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60' alt='project'/>
+            <a href='https://kalees.netlify.app/' target='blank'>
+              <img src='https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60' alt='project'/>
+            </a>
           </div>
           <div className='project-content'>
           <Card className='project-card'>
               <Card.Content header='Pokedex' />
               <Card.Content description='Creation dun pokedex via des requetes a une api.' />
               <Card.Content extra>
-                <Icon name='react' />ReactJS
+              <div className='techno'>
+                  <Icon name='react' />ReactJS
+                </div>
+                <div className='techno'>
+                  <Icon name='js square' />Javascript ES6
+                </div>
+                <div className='techno'>
+                  Web API
+                </div>
               </Card.Content>
               <Card.Content extra>
                 <a href='https://github.com/Gizmo675/pokedex' target='blank'>
                   <Icon name='code' />
                 </a>
-                <a href='https://eloquent-edison-pokedex.netlify.app/' target='blank'>
-                  <Icon name='eye' />
-                </a>
               </Card.Content>
             </Card>
-            <img src='https://images.unsplash.com/photo-1593642703013-5a3b53c965f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60' alt='project'/>
+            <a href='https://eloquent-edison-pokedex.netlify.app/' target='blank'>
+              <img src='https://images.unsplash.com/photo-1593642703013-5a3b53c965f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60' alt='project'/>
+            </a>
           </div>
           <div className='project-content'>
           <Card className='project-card'>
               <Card.Content header='Clone instagram' />
               <Card.Content description='Clone du celebre reseau social instagram.' />
               <Card.Content extra>
-                <Icon name='react' />ReactJS
+              <div className='techno'>
+                  <Icon name='react' />ReactJS
+                </div>
+                <div className='techno'>
+                  <Icon name='js square' />Javascript ES6
+                </div>
+                <div className='techno'>
+                  <Icon name='node js' />NodeJS
+                </div>
+                <div className='techno'>
+                  MongoDB
+                </div>
               </Card.Content>
               <Card.Content extra>
                 <a href='https://github.com/Gizmo675/InstaLike' target='blank'>
                   <Icon name='code' />
-                </a>
-                <a href='#' target='blank'>
-                  <Icon name='eye' />
                 </a>
               </Card.Content>
             </Card>
