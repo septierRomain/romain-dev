b34f94549cab177f9f79dfd38efefa04aa7b0b19
Romain <septier.romain@gmail.com>
2020-09-18 08:59:52 +0200

    adding footer stuf

--- src/Component/Footer/index.js
+++ src/Component/Footer/index.js
@@ -7,8 +7,14 @@ function Footer() {
   return(
     <div id="Footer">
       <h3>
-        <Icon name='creative commons' />
-        Septier Romain. Tous droits réservés.
+        <span className='leftFooter'>
+          Codé avec <Icon name='heart outline' />
+          par Septier Romain.
+        </span>
+        <span className='rightFooter'>
+          <Icon name='creative commons' />
+          Tous droits réservés.
+        </span>
       </h3>
     </div>
   )
