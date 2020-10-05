dfda2c5de5e5e2651933ace39877a4d103e42107
Romain <septier.romain@gmail.com>
2020-10-05 11:18:25 +0200

    adding wave svg

--- src/Component/Home/index.js
+++ src/Component/Home/index.js
@@ -29,7 +29,7 @@ function Home() {
         <hr />
         <p>
           Je suis developpeur web diplomé depuis 2020 et photographe.
-          Je concois et realise des sites web du cahier des charges jusqu'a la mise en ligne. Je peux intervenir seul ou en equipe. Je travaille en collaboration avec des webdesigners. Mes clients sont des sociétes, des artistes et des artisans. 
+          Je conçois et realise des sites web du cahier des charges jusqu'à la mise en ligne. Je peux intervenir seul ou en equipe. Je travaille en collaboration avec des webdesigners. Mes clients sont des sociétes, des artistes et des artisans. 
 
           J'aime les gens passionnées et engagés, j'aime travailler avec eux pour les accompagner dans leurs projets.
         </p>
---
+++ src/Component/SvgComponent/Topwave.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function TopWave(props) {
+  return (
+    <svg width="1221" height="579" viewBox="0 0 1221 579" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <g filter="url(#filter0_d)">
+    <path d="M1216 1H4.99994V550C4.99994 550 146 479 289 474C432 469 641 623 771 550C901 477 948 460 1002 453C1056 446 1216 550 1216 550V1Z" fill="#2C3E50"/>
+    <path d="M1216 1H4.99994V550C4.99994 550 146 479 289 474C432 469 641 623 771 550C901 477 948 460 1002 453C1056 446 1216 550 1216 550V1Z" stroke="url(#paint0_linear)"/>
+    </g>
+    <defs>
+    <filter id="filter0_d" x="0.499939" y="0.5" width="1220" height="577.676" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+    <feOffset dy="4"/>
+    <feGaussianBlur stdDeviation="2"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+    </filter>
+    <linearGradient id="paint0_linear" x1="610.5" y1="1" x2="610.5" y2="569.676" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#D31212"/>
+    <stop offset="1" stop-color="#2230B7" stop-opacity="0"/>
+    </linearGradient>
+    </defs>
+    </svg>
+  )
+}
+
+export default TopWave
+
